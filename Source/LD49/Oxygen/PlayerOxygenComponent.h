57dbb82fe4239999c4beec92c311d01d7b8dfc72
Mizicoca <50930532+TechnicalToucan@users.noreply.github.com>
2021-10-03 00:23:04 +0100

    merge

---
+++ Source/LD49/Oxygen/PlayerOxygenComponent.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "PlayerOxygenComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class LD49_API UPlayerOxygenComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+	static UPlayerOxygenComponent* GetPlayerOxygenComponent(const UObject* WorldContext);
+
+public:
+	UPlayerOxygenComponent();
+
+	virtual void BeginPlay() override;
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;	
+
+private:
+	void TriggerDeath();
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float OxygenLevel = 5000.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float OxygenDepleteRate = 1.0f;
+};
